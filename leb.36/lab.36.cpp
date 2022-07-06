dd263917ef05fc3e375cb7234bf7c7d76faee24a
DESKTOP-IDO1LAQ\RexWang <hank950810@gmail.com>
2022-07-06 20:35:05 +0800

    second commt

---
+++ leb.36/lab.36.cpp
@@ -0,0 +1,42 @@
+#include<iomanip>
+#include<iostream>
+#include<ctime>
+#include<cstdlib>
+using namespace std;
+
+int rand_num ()
+{
+	
+	
+	int num=rand()%100+1;
+	
+	return num;
+	
+}
+
+
+
+int main(){
+    
+    srand(time(NULL));
+    
+   int maze[5][4]={};
+   
+   cout<<"座號"<<" "<<"國文"<<" "<<"英文"<<"　"<<"社會"<<"　"<<"自然"<<endl;
+   
+   for(int i=0;i<5;i++){
+      
+	   cout<<setw(2)<<" "<<i+1;
+	   
+   	for(int j=1;j<=4;j++){
+        maze[i][j]=rand_num();
+             
+            
+	   		cout<<setw(5)<<maze[i][j];
+	   }
+	   cout<<endl;
+   }   
+   
+   
+
+}
